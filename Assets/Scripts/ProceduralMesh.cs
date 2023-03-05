cfd58bb1e1143c7381a7e5fc91f0100511026e8d
Aditya Rajagopal <adi.raj465@gmail.com>
2023-03-05 08:09:29 -0800

    Add gizmos to see vertices, normals, and tangets.

--- Assets/Scripts/ProceduralMesh.cs
+++ Assets/Scripts/ProceduralMesh.cs
@@ -9,6 +9,12 @@ public class ProceduralMesh : MonoBehaviour {
 
     public enum GeneratorType {SquareGrid, SharedSquareGrid, SharedTriangleGrid, PointyHexagonGrid, FlatHexagonGrid};
 
+    [System.Flags]  // by setting system.Flags we ask the following enum to be interpreted as boolean flags. Then we use 1 bit for each flag
+    public enum GizmoMode { Nothing = 0, Vertices = 1, Normals = 0b10, Tangets = 0b100}
+
+    [SerializeField]
+    GizmoMode gizmos;
+
     // static MeshJobScheduleDelegate[,] jobs = {
     //     {
     //         MeshJob<SquareGrid, SingleStream>.ScheduleParallel,
@@ -34,6 +40,10 @@ public class ProceduralMesh : MonoBehaviour {
     [SerializeField]
     GeneratorType generatorType;
 
+    Vector3[] vertices, normals;
+
+    Vector4[] tangents;
+
     Mesh mesh;
 
     private void Awake() {
@@ -49,10 +59,7 @@ public class ProceduralMesh : MonoBehaviour {
     void GenerateMesh() {
        Mesh.MeshDataArray meshDataArray = Mesh.AllocateWritableMeshData(1); // we only want 1 mesh
        Mesh.MeshData meshData = meshDataArray[0];
-    //    int rowsOrHeight = jobs.GetLength(0);
-    //     int colsOrWidth = jobs.GetLength(1);
-    //     Debug.Log("ROws: " + rowsOrHeight);
-    //     Debug.Log("Cols: " + colsOrWidth);
+
        jobs[(int)generatorType](mesh, meshData, resolution, default).Complete(); // default job handel and then when the job handel returns we wait for it to complete.
 
        Mesh.ApplyAndDisposeWritableMeshData(meshDataArray, mesh);
@@ -64,6 +71,53 @@ public class ProceduralMesh : MonoBehaviour {
 		GenerateMesh();
         // Debug.Log((int)generatorType);
 		enabled = false;
+
+        vertices = null;
+        normals = null;
+        tangents = null;
 	}
+
+    void OnDrawGizmos()
+    {
+        if (gizmos == GizmoMode.Nothing || mesh == null){
+            return;
+        }
+
+        bool drawVertices = (gizmos & GizmoMode.Vertices) != 0;
+        bool drawNormals = (gizmos & GizmoMode.Normals) != 0;
+        bool drawTangets = (gizmos & GizmoMode.Tangets) != 0;
+
+        if (vertices == null) {
+            vertices = mesh.vertices;
+        }
+
+        if (drawNormals && normals == null) {
+            normals = mesh.normals;
+        }
+
+        if (drawTangets && tangents == null) {
+            tangents = mesh.tangents;
+        }
+
+        Transform t = transform;
+        for (int i =0; i < vertices.Length; i++) {
+            Vector3 position = t.TransformPoint(vertices[i]);
+
+            if (drawVertices) {
+                Gizmos.color = Color.cyan;
+                Gizmos.DrawSphere(position, 0.05f / resolution);
+            }
+            
+            if (drawNormals) {
+                Gizmos.color = Color.green;
+                Gizmos.DrawRay(position, t.TransformDirection(normals[i]) * 0.5f / resolution);
+            }
+
+            if (drawTangets) {
+                Gizmos.color = Color.red;
+                Gizmos.DrawRay(position, t.TransformDirection(tangents[i]) * 0.5f / resolution);
+            }
+        }
+    }
 
 }
